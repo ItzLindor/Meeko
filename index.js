bf3162e6496452176a09eed2eb4dd609c3f04b4d
OhLindor <lindorrain@gmail.com>
2024-11-05 16:35:33 -0500

    hahaha

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('node:path');
 
 // Require the necessary discord.js classes
 const { Client, Collection, Events, GatewayIntentBits} = require('discord.js');
-const { token } = require('./config.json');
+const { token } = process.env.token;
 
 
 const axios = require('axios');
