c74df62b06ff6022103438d58a81cabb88f5aba5
OhLindor <lindorrain@gmail.com>
2024-11-07 00:48:39 -0500

    leaked

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const fs = require('node:fs');
 const path = require('node:path');
-
+require('dotenv').config()
 
 
 // Require the necessary discord.js classes
 const { Client, Collection, Events, GatewayIntentBits} = require('discord.js');
-const { token } = process.env.token;
+const token = process.env.token;
 
 
 
@@ -125,12 +125,12 @@ async function checkStreamingStatus() {
 
 // Periodically check streaming status every 5 minutes (300000 milliseconds)
 setInterval(checkStreamingStatus, 30000);
-
+console.log(token);
 // Log in to Discord with your client's token
-client.login(process.env.token);
+client.login(token);
 
 
-//client.login("MTMwMzExOTI4OTg2NzU3MTI1MA.GUfy6n.33dJzBNiCPHshV6kohZpeebDZguYykxtIF3zfI");
+
 
 
 
